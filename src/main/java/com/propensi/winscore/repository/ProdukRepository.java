26d3f5957605bfc2816d9eaa24070b59f62be9eb
irfanidianti <ramadianti.irfani9@gmail.com>
2019-03-23 15:36:17 +0700

    add produkService-not finish

--- src/main/java/com/propensi/winscore/repository/ProdukRepository.java
+++ src/main/java/com/propensi/winscore/repository/ProdukRepository.java
@@ -1,8 +1,11 @@
 package com.propensi.winscore.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProdukRepository {
+import com.propensi.winscore.model.ProdukModel;
 
+@Repository
+public interface ProdukRepository extends JpaRepository<ProdukModel, Long> {
+	
 }
--- src/main/java/com/propensi/winscore/service/ProdukService.java
+++ src/main/java/com/propensi/winscore/service/ProdukService.java
@@ -1,5 +1,9 @@
 package com.propensi.winscore.service;
 
-public interface ProdukService {
+import com.propensi.winscore.model.ProdukModel;
 
+public interface ProdukService{
+
+	ProdukModel addNewProduk(Long id_produk);
+	ProdukModel updateProduk(Long id_produk, String namaProduk, String kodeProduk, String detailProduk, String status, Long harga);
 }
